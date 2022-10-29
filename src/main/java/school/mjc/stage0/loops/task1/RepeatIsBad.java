903f2077f07e43d897ee553d411ac764b4da2403
Maryna Kidun <marykidun@gmail.com>
2022-10-29 23:15:15 +0300

    Finished first task

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -3,7 +3,31 @@ package school.mjc.stage0.loops.task1;
 public class RepeatIsBad {
 
     @SuppressWarnings("java:S106")
-    public void repeatIsBad(){
+    public static void repeatIsBad(){
+        String sentence = "writing the same code doesn't have much impact, and it's also time-consuming";
+        System.out.println(sentence);
+        System.out.println(sentence);
+        System.out.println(sentence);
+        System.out.println(sentence);
+        System.out.println(sentence);
+        System.out.println(sentence);
+        System.out.println(sentence);
+        System.out.println(sentence);
+        System.out.println(sentence);
+        System.out.println(sentence);
+        System.out.println(sentence);
+        System.out.println(sentence);
+        System.out.println(sentence);
+        System.out.println(sentence);
+        System.out.println(sentence);
+        System.out.println(sentence);
+        System.out.println(sentence);
+        System.out.println(sentence);
+        System.out.println(sentence);
+        System.out.println(sentence);
 
+    }
+    public static void main(String[] args ) {
+        repeatIsBad();
     }
 }
